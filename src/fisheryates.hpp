05395658b135b2a187b89840c3f86fa3ec6276a9
BLuLaptop <brendannlu5@gmail.com>
2023-06-18 11:32:14 +1000

    commit off lapttop

--- src/fisheryates.hpp
+++ src/fisheryates.hpp
@@ -2,7 +2,17 @@
 #define FISHERYATES_H
 #include "types.hpp"
 
-void FisherYatesShuffle(Card* streamHead, unsigned int shuffleRange) {};
+void FisherYatesShuffle(Card* streamHead, 
+                        unsigned int streamSize,
+                        unsigned int numShuffle
+                        ) {};
+/*
+FisherYates algorithm to shuffle the cardStream in place. 
+numShuffle controls how many shuffled cards we sample in.
+*/
+
+
+
 // we also need rng (trackable source of randomness), how many shuffles (partial-howmany / full)
 
 #endif
