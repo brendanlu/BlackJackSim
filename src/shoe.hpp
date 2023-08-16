612dd58476c19afb47377a0d5878ab51561452d8
BLuLaptop <brendannlu5@gmail.com>
2023-08-16 13:21:08 +1000

    move card related consts into struct defn

--- src/shoe.hpp
+++ src/shoe.hpp
@@ -51,14 +51,6 @@ private:
 
     static constexpr unsigned int MAX_DECKS = 8;
     static constexpr unsigned int DECK_SIZE = 52;
-    static constexpr unsigned int N_CARD_FACE_VALS = 13;
-    static constexpr unsigned int N_CARD_SUIT_VALS = 4;
-
-    static constexpr std::array<char, N_CARD_FACE_VALS> 
-    FACE_VALS = {'2','3','4','5','6','7','8','9','T','J','Q','K','A'};
-
-    static constexpr std::array<char, N_CARD_SUIT_VALS> 
-    SUIT_VALS = {'D', 'S', 'C', 'H'};
 
     std::array<Card, MAX_DECKS*DECK_SIZE> cardStream; 
 };
--- src/types.hpp
+++ src/types.hpp
@@ -1,6 +1,7 @@
 #ifndef TYPES_H
 #define TYPES_H 
 
+#include <array>
 #include <iostream>
 
 /*
@@ -8,7 +9,6 @@ misc. constants
 */
 constexpr unsigned int BJVAL = 21; 
 constexpr unsigned int INIT_DEAL = 2;
-constexpr unsigned int VALS[10] = {2,3,4,5,6,7,8,9,10,11};
 constexpr int ASCII_2 = 50; // ascii value of 2 
 constexpr int ASCII_9 = 57;
 constexpr int ASCII_A = 65; 
@@ -17,6 +17,19 @@ constexpr int ASCII_A = 65;
 A simple struct to represent a card with minimal memory overhead and efficient
 value retrieval, whilst maintaining full face and suit information. 
 */
+constexpr unsigned int N_CARD_FACE_VALS = 13;
+constexpr unsigned int N_DISTINCT_NUMERIC = 10;
+constexpr unsigned int N_CARD_SUIT_VALS = 4;
+
+constexpr std::array<char, N_CARD_FACE_VALS> 
+FACE_VALS = {'2','3','4','5','6','7','8','9','T','J','Q','K','A'};
+
+constexpr std::array<unsigned int, N_DISTINCT_NUMERIC>
+NUMERIC_VALS = {2,3,4,5,6,7,8,9,10,11};
+
+constexpr std::array<char, N_CARD_SUIT_VALS> 
+SUIT_VALS = {'D','S','C','H'};
+
 struct Card {
     char face; 
     char suit;
@@ -28,15 +41,15 @@ struct Card {
     {
         // 2-9
         if ((int)face <= ASCII_9) {
-            return VALS[(int)face - ASCII_2];
+            return NUMERIC_VALS[(int)face - ASCII_2];
         }
         // A
         else if (face == 'A') {
-            return VALS[9];
+            return NUMERIC_VALS[9];
         }
         // T, J, Q, K
         else {
-            return VALS[8];
+            return NUMERIC_VALS[8];
         }
     }
 
