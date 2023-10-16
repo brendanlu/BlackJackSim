4b548a333a914b6585051f4e939b9f89a25c677e
Brendan Lu <brendannlu5@gmail.com>
2023-10-16 21:03:50 +1100

    minor: tidy

--- tests/clean.py
+++ tests/clean.py
@@ -1,35 +1,10 @@
 """
-This clunky script will clean both this directory and the python module 
-after building and test-runs. 
+Standlone clean
 """
 
-import os
-import shutil
+from test_simulation import make_clean_build_env, make_clean_dir
 
-MODULE_DIR = "../cardstream/"
 
-# remove build folder
-try:
-    shutil.rmtree("../build")
-    print("Removed build folder")
-except FileNotFoundError:
-    pass
-
-# Cython generated C++ file
-try:
-    os.remove(MODULE_DIR + "_wrappers.cpp")
-    print("Removed Cython generated C++ file")
-except FileNotFoundError:
-    pass
-
-# remove cython generated files, and compiled extensions
-for file in os.listdir(MODULE_DIR):
-    if file.endswith(".pyd"):
-        os.remove(MODULE_DIR + file)
-        print("Removed", file)
-
-# clean from a simulation run
-for file in os.listdir():
-    if "LOG" in file or "ERROR" in file:
-        os.remove(file)
-        print("Removed", file)
+if __name__ == "__main__": 
+    make_clean_build_env() 
+    make_clean_dir
