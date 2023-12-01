a8c3a2f5ea6950cd5146a485d1f37f6fe60300f0
BLuLaptop <brendannlu5@gmail.com>
2023-12-01 14:08:36 +1100

    in progress

--- cardstream/__init__.py
+++ cardstream/__init__.py
@@ -1,4 +1,3 @@
-# testing internals for now
 from ._utils import _strat_to_numpy_arrayfmt
 from . import _wrappers
 
--- devel/clean.py
+++ devel/clean.py
@@ -2,7 +2,7 @@
 Standlone clean
 """
 
-from test_simulation import make_clean_build_env, make_clean_dir
+from test_base_wrapper import make_clean_build_env, make_clean_dir
 
 
 if __name__ == "__main__":
